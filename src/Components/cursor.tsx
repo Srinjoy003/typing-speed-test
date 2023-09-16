124c58e08b43d01b12612459c07fa549e5d09fe9
Srinjoy003 <srinjoy003@gmail.com>
2023-09-16 21:10:01 +0530

    yes

--- src/Components/cursor.tsx
+++ src/Components/cursor.tsx
@@ -1,46 +1,76 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 type CursorProp = {textRef ?: any; translatePos?: Array<number>; xJump?: number; newLine?: Array<number>; colour?: string};
   
-// function getTextWidth(text: string) {
-//   const canvas = document.createElement('canvas');
-//   const context = canvas.getContext('2d');
-//   context.font = window.getComputedStyle(textContainer).getPropertyValue('font');
-//   const width = context.measureText(text).width;
-//   return width;
-// }
 
 
 function Cursor({ xJump, textRef}: CursorProp) {
     const [translateX, setTranslateX] = useState(0);
+    const [widthList, setWidthList] = useState<number[]>([]);
+    const [jump, setJump] = useState<number>(0);
+    const [jumpIndex, setJumpIndex] = useState(0);
 
-    const jump = xJump || 15;
-    const handleKeyPress = (event: KeyboardEvent) => {
-        if (event.code === "Space") {
-        // Adjust the value to control the amount of movement
-        setTranslateX((prevTranslateX) => prevTranslateX + jump);
-        console.log(translateX);
-        }
-    };
-
-
+    const cursorRef = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
-    console.log("Effect is running!"); // Add this line
-    document.addEventListener("keydown", handleKeyPress);
-
-    // Clean up the event listener when the component unmounts
-    return () => {
-        document.removeEventListener("keydown", handleKeyPress);
-    };
+      if (cursorRef.current) {
+        cursorRef.current.style.transform = `translateX(${translateX}px) translateY(-133px)`;
+      }
     }, [translateX]);
+
+    useEffect(() => {
+      const textDiv: HTMLDivElement = textRef.current;
+      const spanList = textDiv.querySelectorAll("span");
+      const newWidthList = Array.from(spanList).map((span) => {
+        return span.getBoundingClientRect().width;
+      }).slice(1);
+
+      setWidthList(newWidthList);
+    
+    }, [textRef]);
+
+
+    
+
+    useEffect(() => {
+      console.log("Effect is running!"); // Add this line
+      document.addEventListener("keydown", handleKeyPress);
+      
+      // Clean up the event listener when the component unmounts
+      return () => {
+          document.removeEventListener("keydown", handleKeyPress);
+      };
+      }, [widthList, jumpIndex]);
+
+  
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.code === "Space") {
+
+        if (widthList.length > jumpIndex) {
+      // Adjust the value to control the amount of movement
+          setJump(widthList[jumpIndex])
+
+          setTranslateX((prevTranslateX) => {
+            return prevTranslateX + jump;
+          });
+          
+          setJumpIndex((curIndex) => {
+            return curIndex + 1;
+          });
+
+          console.log()
+          
+        }
+      }
+    };
+
 
   // Define the style for the div based on translateY
  
 
     const modifiedClass = "absolute border-[1.5px] h-6 border-arch-cursor";
     return (
-      <div className={modifiedClass} style={{ transform: `translateX(${translateX}px) translateY(-133px)` }}></div>
+      <div ref={cursorRef} className={modifiedClass}></div>
     );
 }
 
