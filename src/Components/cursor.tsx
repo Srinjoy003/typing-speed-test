24234842c55e1565f40892ebf5ea35781fba0b85
Srinjoy003 <srinjoy003@gmail.com>
2023-09-17 10:41:53 +0530

    yes

--- src/Components/cursor.tsx
+++ src/Components/cursor.tsx
@@ -6,26 +6,35 @@ type CursorProp = {textRef ?: any; translatePos?: Array<number>; xJump?: number;
 
 function Cursor({ xJump, textRef}: CursorProp) {
     const [translateX, setTranslateX] = useState(0);
-    const [widthList, setWidthList] = useState<number[]>([]);
+    const [translateY, setTranslateY] = useState(-134); //-133 -97
+    const [widthList, setWidthList] = useState<any>([]);
     const [jump, setJump] = useState<number>(0);
     const [jumpIndex, setJumpIndex] = useState(0);
+    const [lineIndex, setLineIndex] = useState(0);
+
 
     const cursorRef = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
       if (cursorRef.current) {
-        cursorRef.current.style.transform = `translateX(${translateX}px) translateY(-133px)`;
+        cursorRef.current.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`;
       }
-    }, [translateX]);
+    }, [translateX, translateY]);
 
     useEffect(() => {
       const textDiv: HTMLDivElement = textRef.current;
-      const spanList = textDiv.querySelectorAll("span");
-      const newWidthList = Array.from(spanList).map((span) => {
-        return span.getBoundingClientRect().width;
-      }).slice(1);
+      const outerSpans = textDiv.querySelectorAll("span"); // Select outer spans
+
+      const newWidthList = Array.from(outerSpans).map((outerSpan) => {
+        const innerSpans = outerSpan.querySelectorAll("span"); // Select nested spans within the outer span
+        const widths = Array.from(innerSpans).map((span) => span.getBoundingClientRect().width);
+        return widths.length > 0 ? widths : null;
+      }).filter(Boolean); // Filter out null values
+     
+      
 
       setWidthList(newWidthList);
+
     
     }, [textRef]);
 
@@ -40,32 +49,55 @@ function Cursor({ xJump, textRef}: CursorProp) {
       return () => {
           document.removeEventListener("keydown", handleKeyPress);
       };
-      }, [widthList, jumpIndex]);
+      }, [widthList, jumpIndex, lineIndex]);
 
   
     const handleKeyPress = (event: KeyboardEvent) => {
       if (event.code === "Space") {
 
-        if (widthList.length > jumpIndex) {
-      // Adjust the value to control the amount of movement
-          setJump(widthList[jumpIndex])
-
+        
+        if (lineIndex >= widthList.length) {
+          // Reset lineIndex and jumpIndex when we reach the end
+          setLineIndex(0);
+          setJumpIndex(0);
+        }
+    
+        const currentLine = widthList[lineIndex];
+    
+        if (currentLine && currentLine.length > jumpIndex) {
+          setJump(currentLine[jumpIndex]);
+    
           setTranslateX((prevTranslateX) => {
             return prevTranslateX + jump;
           });
-          
+    
           setJumpIndex((curIndex) => {
             return curIndex + 1;
           });
+    
+          if (jumpIndex >= currentLine.length - 1) {
+            setJumpIndex(0);
+            setLineIndex((curLineIndex) => {
+              return curLineIndex + 1;
+            });
 
-          console.log()
-          
+            setTranslateY((prevTranslateY) => {
+              return prevTranslateY + 37;
+            });
+
+            setTranslateX(0);
+             
+
+          }
+    
+          // console.log(lineIndex, jumpIndex, translateX, jump);
         }
+    
+    
       }
     };
 
 
-  // Define the style for the div based on translateY
  
 
     const modifiedClass = "absolute border-[1.5px] h-6 border-arch-cursor transition-transform duration-200";
