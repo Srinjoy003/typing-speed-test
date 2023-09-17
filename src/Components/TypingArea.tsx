0d63367b7e329c96e95e02c4b5829b5f0a97d646
Srinjoy003 <srinjoy003@gmail.com>
2023-09-17 23:56:12 +0530

    z

--- src/Components/TypingArea.tsx
+++ src/Components/TypingArea.tsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react";
 import Cursor from "./cursor";
 
-type textAreaProp = {textColour: string};
+type textAreaProp = { textColour: string };
 
 function CharacterSeparator(lineList: Array<Array<string>>) {
   let charList = [];
@@ -31,11 +31,9 @@ function LineSeparator(wordList: Array<string>, charCount: number) {
       if (counter == charCount) {
         let line = wordList.slice(prevIndex, i);
 
-        if (line[line.length - 1] === " ") 
-          line.pop();
-        
-        if (line[0] == " ")
-          line = line.slice(1);
+        if (line[line.length - 1] === " ") line.pop();
+
+        if (line[0] == " ") line = line.slice(1);
 
         lineList.push(line);
         counter = j;
@@ -47,37 +45,9 @@ function LineSeparator(wordList: Array<string>, charCount: number) {
   return lineList;
 }
 
-function CreateFinalDiv(){
-  
-  const randomWords = [
-    "apple",
-    "banana",
-    "chocolate",
-    "dog",
-    "elephant",
-    "flower",
-    "guitar",
-    "happiness",
-    "internet",
-    "jazz",
-    "kangaroo",
-    "lighthouse",
-    "mountain",
-    "notebook",
-    "ocean",
-    "penguin",
-    "quasar",
-    "rainbow",
-    "sunset",
-    "tiger",
-    "umbrella",
-    "volcano",
-    "watermelon",
-    "xylophone",
-    "yogurt",
-    "zeppelin",
-  ];
-  
+function CreateFinalDiv() {
+  const randomWords = ["apple", "banana", "chocolate", "dog", "elephant", "flower", "guitar", "happiness", "internet", "jazz", "kangaroo", "lighthouse", "mountain", "notebook", "ocean", "penguin", "quasar", "rainbow", "sunset", "tiger", "umbrella", "volcano", "watermelon", "xylophone", "yogurt", "zeppelin"];
+
   let len = randomWords.length;
   let wordCount = 36;
   let lineCount = 4;
@@ -88,31 +58,24 @@ function CreateFinalDiv(){
     let randomIndex = Math.floor(Math.random() * len) % wordCount;
     wordList.push(randomWords[randomIndex]);
     wordList.push(" ");
-
   }
 
   wordList.pop();
-  
-  let lineList = LineSeparator(wordList, 60).slice(0,lineCount);
+
+  let lineList = LineSeparator(wordList, 60).slice(0, lineCount);
   let finalList = CharacterSeparator(lineList);
   finalList = finalList.map((row) => {
     return row.map((str) => {
       return str.replace(" ", spaceChar);
-    })
+    });
   });
-  
+
   let finalDiv = finalList.map((subArray, rowIndex) => {
     let subSpan = subArray.map((character, colIndex) => {
-      
       if (character === spaceChar) {
-        return (
-        <span key={colIndex} dangerouslySetInnerHTML={{ __html: spaceChar }}></span>);
-      } 
-      
-      else {
-        return (
-          <span key={colIndex}>{character}</span>
-        );
+        return <span key={colIndex} dangerouslySetInnerHTML={{ __html: spaceChar }}></span>;
+      } else {
+        return <span key={colIndex}>{character}</span>;
       }
     });
 
@@ -122,17 +85,16 @@ function CreateFinalDiv(){
   return finalDiv;
 }
 
-
-function TypingArea({textColour}: textAreaProp) {
-  
-  const [finalDiv, setFinalDiv] = useState(() =>  CreateFinalDiv())
+function TypingArea({ textColour }: textAreaProp) {
+  const [finalDiv, setFinalDiv] = useState(() => CreateFinalDiv());
+  const [jumpIndex, setJumpIndex] = useState(0);
+  const [lineIndex, setLineIndex] = useState(0);
 
   // let finalDiv = CreateFinalDiv();
   const textDivRef = useRef<HTMLDivElement | null>(null);
 
-
   //------------------------------------------------------------------------------------------------------------------
-  
+
   // useEffect(() => {
   //   if (cursorRef.current) {
   //     cursorRef.current.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`;
@@ -141,7 +103,7 @@ function TypingArea({textColour}: textAreaProp) {
 
   // useEffect(() => {
   //   const textDiv: HTMLDivElement | null = textDivRef.current;
-    
+
   //   if(textDiv){
   //     const outerSpans = textDiv.querySelectorAll("span"); // Select outer spans
 
@@ -152,51 +114,43 @@ function TypingArea({textColour}: textAreaProp) {
   //     }).filter(Boolean); // Filter out null values
 
   // }
-   
-    
 
   //   setWidthList(newWidthList);
 
-  
-  // }, [textDivRef]);
-
-
-  
+  // }, [finalDiv]);
 
   // useEffect(() => {
   //   console.log("Effect is running!"); // Add this line
   //   document.addEventListener("keydown", handleKeyPress);
-    
+
   //   // Clean up the event listener when the component unmounts
   //   return () => {
   //       document.removeEventListener("keydown", handleKeyPress);
   //   };
   //   }, [widthList, jumpIndex, lineIndex]);
 
-
   // const handleKeyPress = (event: KeyboardEvent) => {
   //   if (event.code === "Space") {
 
-      
   //     if (lineIndex >= widthList.length) {
   //       // Reset lineIndex and jumpIndex when we reach the end
   //       setLineIndex(0);
   //       setJumpIndex(0);
   //     }
-  
+
   //     const currentLine = widthList[lineIndex];
-  
+
   //     if (currentLine && currentLine.length > jumpIndex) {
   //       setJump(currentLine[jumpIndex]);
-  
+
   //       setTranslateX((prevTranslateX) => {
   //         return prevTranslateX + jump;
   //       });
-  
+
   //       setJumpIndex((curIndex) => {
   //         return curIndex + 1;
   //       });
-  
+
   //       if (jumpIndex >= currentLine.length - 1) {
   //         setJumpIndex(0);
   //         setLineIndex((curLineIndex) => {
@@ -208,28 +162,26 @@ function TypingArea({textColour}: textAreaProp) {
   //         });
 
   //         setTranslateX(0);
-           
 
   //       }
-  
+
   //       // console.log(lineIndex, jumpIndex, translateX, jump);
   //     }
-  
-  
+
   //   }
   // };
 
-//---------------------------------------------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------------------------------------------------
   const modifiedClass = `flex flex-col text-3xl tracking-widest w-fit h-fit ${textColour}`;
 
-  
   return (
     <div className="w-3/4">
       <input type="text" className="absolute hidden w-40 h-40 bg-gray-300 left-10"></input>
-      <div ref={textDivRef} className={modifiedClass}>{finalDiv}</div>
+      <div ref={textDivRef} className={modifiedClass}>
+        {finalDiv}
+      </div>
 
-      
-      <Cursor textRef={textDivRef}/>
+      <Cursor textRef={textDivRef} />
     </div>
   );
 }
